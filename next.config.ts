88f29f65f7ce2a3a390b97619604353fead61608
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-03-13 19:54:23 +0700

    Update the config

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,16 @@ import type { NextConfig } from 'next'
 import withNextIntl from 'next-intl/plugin'
 
 const nextConfig: NextConfig = withNextIntl()({
-  /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'utfs.io',
-        port: '',
-      },
-    ],
-  },
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'utfs.io',
+				port: '',
+			},
+		],
+		deviceSizes: [640, 750, 828, 1080, 1200, 1400, 1536, 1920, 2048, 3840],
+	},
 })
 
 export default nextConfig
