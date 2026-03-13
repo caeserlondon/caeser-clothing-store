b965237d70683ddd1492c18e576ebdcf20f13e20
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-03-13 19:56:58 +0700

    Refactor to improve Lighthouse and web vitals

--- types/index.ts
+++ types/index.ts
@@ -1,65 +1,65 @@
 import {
-  CarouselSchema,
-  CartSchema,
-  DeliveryDateSchema,
-  OrderInputSchema,
-  OrderItemSchema,
-  PaymentMethodSchema,
-  ProductInputSchema,
-  ReviewInputSchema,
-  SettingInputSchema,
-  ShippingAddressSchema,
-  SiteCurrencySchema,
-  SiteLanguageSchema,
-  UserInputSchema,
-  UserNameSchema,
-  UserSignInSchema,
-  UserSignUpSchema,
-  WebPageInputSchema,
+	CarouselSchema,
+	CartSchema,
+	DeliveryDateSchema,
+	OrderInputSchema,
+	OrderItemSchema,
+	PaymentMethodSchema,
+	ProductInputSchema,
+	ReviewInputSchema,
+	SettingInputSchema,
+	ShippingAddressSchema,
+	SiteCurrencySchema,
+	SiteLanguageSchema,
+	UserInputSchema,
+	UserNameSchema,
+	UserSignInSchema,
+	UserSignUpSchema,
+	WebPageInputSchema,
 } from '@/lib/validator'
 import { z } from 'zod'
 
 export type IReviewInput = z.infer<typeof ReviewInputSchema>
 export type IReviewDetails = IReviewInput & {
-  _id: string
-  createdAt: string
-  user: {
-    name: string
-  }
+	_id: string
+	createdAt: string
+	user: {
+		name: string
+	}
 }
 export type IProductInput = z.infer<typeof ProductInputSchema>
 
 export type Data = {
-  settings: ISettingInput[]
-  webPages: IWebPageInput[]
-  users: IUserInput[]
-  products: IProductInput[]
-  reviews: {
-    title: string
-    rating: number
-    comment: string
-  }[]
-  headerMenus: {
-    name: string
-    href: string
-  }[]
-  carousels: {
-    image: string
-    url: string
-    title: string
-    buttonCaption: string
-    isPublished: boolean
-  }[]
+	settings: ISettingInput[]
+	webPages: IWebPageInput[]
+	users: IUserInput[]
+	products: IProductInput[]
+	reviews: {
+		title: string
+		rating: number
+		comment: string
+	}[]
+	headerMenus: {
+		name: string
+		href: string
+	}[]
+	carousels: {
+		image: string
+		url: string
+		title: string
+		buttonCaption: string
+		isPublished: boolean
+	}[]
 }
 // Order
 export type IOrderInput = z.infer<typeof OrderInputSchema>
 export type IOrderList = IOrderInput & {
-  _id: string
-  user: {
-    name: string
-    email: string
-  }
-  createdAt: Date
+	_id: string
+	user: {
+		name: string
+		email: string
+	}
+	createdAt: Date
 }
 export type OrderItem = z.infer<typeof OrderItemSchema>
 export type Cart = z.infer<typeof CartSchema>
@@ -75,10 +75,10 @@ export type IUserName = z.infer<typeof UserNameSchema>
 export type IWebPageInput = z.infer<typeof WebPageInputSchema>
 
 // setting
-export type ICarousel = z.infer<typeof CarouselSchema>
+export type Carousel = z.infer<typeof CarouselSchema>
 export type ISettingInput = z.infer<typeof SettingInputSchema>
 export type ClientSetting = ISettingInput & {
-  currency: string
+	currency: string
 }
 export type SiteLanguage = z.infer<typeof SiteLanguageSchema>
 export type SiteCurrency = z.infer<typeof SiteCurrencySchema>
